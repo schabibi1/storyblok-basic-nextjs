f6e037c1cc67669959cda88c21b6b2ac15799d03
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-05-03 20:16:12 +0200

    add: image field in Teaser after my talk

--- components/Teaser.js
+++ components/Teaser.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Teaser = ({blok}) => {
-  console.log(blok)
+  console.log(blok);
   return (
     <div>
       <h2>{blok.headline}</h2>
+      <img src={blok.image.filename} alt={blok.image.alt} />
     </div>
   )
 }
